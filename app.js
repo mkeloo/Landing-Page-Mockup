89b52a3333c417025d00e6b190c5e3ea77a12b89
mkeloo <mkeloo@ufl.edu>
2022-08-31 14:01:54 -0400

    Added Theme Toggle function

--- app.js
+++ app.js
@@ -1,4 +1,52 @@
-// Select The Elements
+// Select the Elements
 var toggle_btn
 var big_wrapper
-var hamburger_menu
+
+function declare() {
+  toggle_btn = document.querySelector(".toggle-btn")
+  big_wrapper = document.querySelector(".big-wrapper")
+}
+
+const main = document.querySelector("main")
+declare()
+
+let dark = false
+
+function toggleAnimation() {
+  dark = !dark
+
+  // Clone the wrapper (landing page)
+  let clone = big_wrapper.cloneNode(true)
+
+  // Changing the color
+  if (dark) {
+    clone.classList.remove("light")
+    clone.classList.add("dark")
+  } else {
+    clone.classList.remove("dark")
+    clone.classList.add("light")
+  }
+
+  // Cloning the other theme
+  clone.classList.add("copy")
+  main.appendChild(clone)
+
+  //   Stop-Scrolling
+  document.body.classList.add("stop-scrolling")
+
+  // removing unwanted big_Wrapper after the animation
+  clone.addEventListener("animationend", () => {
+    document.body.classList.remove("stop-scrolling")
+    big_wrapper.remove()
+    clone.classList.remove("copy")
+
+    // Reset the Variables
+    declare()
+    events()
+  })
+}
+
+function events() {
+  toggle_btn.addEventListener("click", toggleAnimation)
+}
+events()
