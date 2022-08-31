ab62a1cbb0596baf40de3cb4a55dafe0874c45ff
mkeloo <mkeloo@ufl.edu>
2022-08-31 14:50:09 -0400

    Completed the Final version

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Select the Elements
 var toggle_btn
 var big_wrapper
+var hamburger_menu
 
 function declare() {
   toggle_btn = document.querySelector(".toggle-btn")
   big_wrapper = document.querySelector(".big-wrapper")
+  hamburger_menu = document.querySelector(".hamburger-menu")
 }
 
 const main = document.querySelector("main")
@@ -48,5 +50,8 @@ function toggleAnimation() {
 
 function events() {
   toggle_btn.addEventListener("click", toggleAnimation)
+  hamburger_menu.addEventListener("click", () => {
+    big_wrapper.classList.toggle("active")
+  })
 }
 events()
